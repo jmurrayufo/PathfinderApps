bdf273cad2a1a23b5ce541cff138296eceeb77db
John Murray <jmurrayufo@gmail.com>
2014-10-06 09:02:08 -0600

    Added LootGenerator

---
+++ SpellBook/LootGen.py
@@ -0,0 +1,42 @@
+import csv 
+import random
+
+sourceFilterList = [ 'Ultimate Magic', 'Ultimate Combat', 'Advanced Race Guide',
+   'APG', 'PFRPG Core', ]
+
+classes = [ 
+      'sor',
+      'wiz',
+      'cleric',
+      'druid',
+      'ranger',
+      'bard',
+      'paladin',
+      'alchemist',
+      'summoner',
+      'witch',
+      'inquisitor',
+      'oracle',
+      'antipaladin',
+      'magus',
+   ]
+
+randClass = random.choice( classes ) 
+
+with open( 'spell_full.csv' ) as fp:
+   csvreader = csv.DictReader( fp )
+   columns = csvreader.fieldnames
+   spellDB = []
+   for i in csvreader:
+      # Append the converted row to the spell DB
+      spellDB.append( i )
+
+spellDBFiletered = [ i for i in spellDB if ( i[ 'source' ] in sourceFilterList and i[ randClass ] in ['1','2','3','4'] ) ]
+
+selection = random.sample( spellDBFiletered, 2)
+
+print "Class: {}".format( randClass )
+for i in selection :
+   print i['name'],"[{}]".format( i[randClass])
+   print " ",i['short_description']
+   print
