571c7ac66868f58aad6ceed544bdc3625406d450
John Murray <jmurrayufo@gmail.com>
2014-10-13 22:11:25 -0600

    Make Weighted dicey work beyond ~29K runs.

--- Dice/WeightedDice.py
+++ Dice/WeightedDice.py
@@ -3,34 +3,33 @@
 import numpy as np
 import time
 
-sides = {}
+p = np.ones(20,dtype=float)
+p /= sum( p )
 
-for i in range(1,21):
-   sides[i] = 1.0
+count = np.zeros(20,dtype=int)
 
-count = np.zeros(20)
+totalLoops = 100000
+expected = 0
 
-p = np.array([sides[x] for x in sides])
-p /= sum(p)
+for i in range( totalLoops ) :
 
-
-for i in range(500000):
-   result = np.random.choice(sides.keys(),p=p)
-   count[ result-1 ] += 1
-   continue
-   sides[ result ] *= 0.95 
-   # Recalculate Prob
-   p = np.array([sides[x] for x in sides])
-   p /= sum(p)
-   continue
-   print result
-   print p
+   side = np.random.choice( range(0,20), 1, p=p )[0]
+   idx = np.where( p == max(p) )[0][0]
+   # print "{:2} {:6.2%}    expected {:2} {:6.2%}".format( side+1, p[side], idx+1, p[idx]  )
+   print "{:2}".format( side + 1 )
+   if p[side] > 0.05 :
+      expected += 1
+   count[side] += 1
+   p[side] *= 0.1
+   p /= sum( p ) 
    time.sleep(1)
+   
+
+print count
 print p
 
-for idx,val in enumerate( count ):
-   print "{:2}: {:,.0f}".format( idx,val )
-
-print "std: {}".format( np.std(count ) )
-print "min: {}".format( np.min(count ) )
-print "max: {}".format( np.max(count ) )
+print "STD:",np.std( count )
+print "MAX:",np.max( count )
+print "MIN:",np.min( count )
+print "DIF:",np.max( count ) - np.min( count )
+print "EXP:",expected/float(totalLoops)
