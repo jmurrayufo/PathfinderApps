36d36957d0761211395ca0ed1399a8f17551d3ce
John Murray <jmurrayufo@gmail.com>
2014-10-14 01:17:18 -0600

    Weighted dice testing

--- Dice/WeightedDice.py
+++ Dice/WeightedDice.py
@@ -3,33 +3,45 @@
 import numpy as np
 import time
 
-p = np.ones(20,dtype=float)
+sided = 10
+
+p = np.ones(sided,dtype=float)
 p /= sum( p )
 
-count = np.zeros(20,dtype=int)
+count = np.zeros(sided,dtype=int)
+base  = np.zeros(sided,dtype=int)
 
-totalLoops = 100000
+totalLoops = 10000
 expected = 0
 
 for i in range( totalLoops ) :
 
-   side = np.random.choice( range(0,20), 1, p=p )[0]
+   side = np.random.choice( range(0,sided), 1, p=p )[0]
+   side2 = np.random.choice(range(0,sided))
    idx = np.where( p == max(p) )[0][0]
    # print "{:2} {:6.2%}    expected {:2} {:6.2%}".format( side+1, p[side], idx+1, p[idx]  )
-   print "{:2}".format( side + 1 )
+   print "{:2} {:2}".format( side + 1, side2 + 1 )
    if p[side] > 0.05 :
       expected += 1
    count[side] += 1
+   base[side2] += 1
+
    p[side] *= 0.1
+   
    p /= sum( p ) 
-   time.sleep(1)
+   time.sleep(0.1)
    
 
-print count
-print p
-
+# print count
+# print p
+print "\nWeighted:"
 print "STD:",np.std( count )
 print "MAX:",np.max( count )
 print "MIN:",np.min( count )
 print "DIF:",np.max( count ) - np.min( count )
-print "EXP:",expected/float(totalLoops)
+
+print "\nBalanced:"
+print "STD:",np.std( base )
+print "MAX:",np.max( base )
+print "MIN:",np.min( base )
+print "DIF:",np.max( base ) - np.min( base )
