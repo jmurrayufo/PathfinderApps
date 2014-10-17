14bb107f6199746eee4ab70aa0b880cd85651e1d
John Murray <jmurrayufo@gmail.com>
2014-10-16 21:17:55 -0600

    Added check to prevent oscilation of die

--- Dice/D20.py
+++ Dice/D20.py
@@ -1,50 +1,64 @@
+#!/usr/bin/env python
 import numpy as np
 from numpy.random import random, choice, random_integers
 import time
 
 mapping = {}
-mapping[1] =  [ 7,19,13]
+mapping[1] =  [ 7,13,19]
 mapping[2] =  [12,18,20]
-mapping[3] =  [17,16,19]
-mapping[4] =  [11,18,14]
+mapping[3] =  [16,17,19]
+mapping[4] =  [11,14,18]
 mapping[5] =  [13,15,18]
-mapping[6] =  [14, 9,16]
+mapping[6] =  [ 9,14,16]
 mapping[7] =  [ 1,15,17]
 mapping[8] =  [10,16,20]
 mapping[9] =  [ 6,11,19]
-mapping[10] = [12, 8,17]
+mapping[10] = [ 8,12,17]
 mapping[11] = [ 4, 9,13]
-mapping[12] = [15, 2,10]
-mapping[13] = [11, 5, 1]
-mapping[14] = [20, 4, 6]
+mapping[12] = [ 2,10,15]
+mapping[13] = [ 1, 5,11]
+mapping[14] = [ 4, 6,20]
 mapping[15] = [ 5, 7,12]
-mapping[16] = [ 8, 3, 6]
-mapping[17] = [10, 3, 7]
+mapping[16] = [ 3, 6, 8]
+mapping[17] = [ 3, 7,10]
 mapping[18] = [ 2, 4, 5]
 mapping[19] = [ 1, 9, 3]
-mapping[20] = [ 2,14, 8]
+mapping[20] = [ 2, 8,14]
 
 def GetRandomSide():
    global mapping
    side = choice( mapping.keys() )
+   last = side
    for i in range( random_integers(1,20) ):
-      side = choice( mapping[side])
+      tmplist = [x for x in mapping[side] if x != last]
+      last = side
+      side = choice( tmplist )
    return side
 
+"""
+print "Begin"
 speed = 10
 side = choice( mapping.keys() )
-while speed > 0.4 :
+last = side
+while speed > 0.3 :
+   tmplist = [x for x in mapping[side] if x != last]
+   last = side
+   side = choice( tmplist )
+
    print "\n{:2}".format( side )
    time.sleep(1.0/speed)
-   speed -= random()
-   side = choice( mapping[side] )
+   speed -= random()*2
 
-# results = []
-# for i in range( 1000 ):
-#    results.append( GetRandomSide() )
 
-# for i in range(1,21):
-#    print "{:2}:{:6,}".format(i,results.count(i))
 
-# print np.std( results )
-# print np.mean( results )
+print "Almost got:",mapping[side]
+"""
+results = []
+for i in range( 1000000 ):
+   results.append( GetRandomSide() )
+
+for i in range(1,21):
+   print "{:2}:{:6,}".format(i,results.count(i))
+
+print np.std( results )
+print np.mean( results )
