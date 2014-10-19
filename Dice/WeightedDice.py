215a373ecb8be3bf79cf237f7258c9b62eeb1927
John Murray <jmurrayufo@gmail.com>
2014-10-18 18:46:15 -0600

    Now accept user input

--- Dice/WeightedDice.py
+++ Dice/WeightedDice.py
@@ -10,12 +10,33 @@ p /= sum( p )
 
 count = np.zeros(sided,dtype=int)
 
+def ShowStats( ):
+   global count
+   global p
+   global loops
+   
+   for idx,(x,y) in enumerate( zip( count, p ) ):
+      print "#{:2} Count: {:2}  Odds: {:.2%}".format(idx+1,x,y)
+   print "STD:",np.std( count )
+   print "MAX:",np.max( count )
+   print "MIN:",np.min( count )
+   print "DIF:",np.max( count ) - np.min( count )
+   print "Loops:",loops
+   
 
 loops = 0
 rollover = 0
 
+print "Press Enter to roll..."
+
 try:
    while 1 :
+      user_sel = raw_input()
+
+      if user_sel in ['u'] :
+         ShowStats()
+         continue
+
       loops += 1
       side = np.random.choice( range(0,sided), 1, p=p )[0]
       idx = np.where( p == max(p) )[0][0]
@@ -25,22 +46,13 @@ try:
          print "{:2} [{:5,.1%}]   Expected: {:2} [{:5,.1%}]".format( side+1, p[side], idx+1, p[idx])
       count[side] += 1
 
-      p[side] *= 0.5
+      p[side] *= 0.7 + np.random.random()*0.1
       
       p /= sum( p ) 
-      time.sleep(0.5)
+
+      #time.sleep(0.01)
       
 except (KeyboardInterrupt) :
    pass
    
-
-print count
-print p
-print "\nLeft Side:"
-print "STD:",np.std( count )
-print "MAX:",np.max( count )
-print "MIN:",np.min( count )
-print "DIF:",np.max( count ) - np.min( count )
-
-
-print "Loops:",loops
+ShowStats()
