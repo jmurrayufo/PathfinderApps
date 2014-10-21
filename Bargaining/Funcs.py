ec70a5e66fa0be57949b681c7abce0c0ecbea576
John Murray <jmurrayufo@gmail.com>
2014-10-20 20:43:15 -0600

    Corrected odd overflow with better solution

--- Bargaining/Funcs.py
+++ Bargaining/Funcs.py
@@ -3,7 +3,16 @@ import numpy as np
 from math import log10, floor
 
 def Round_to_n(x,n=1):
-   return round( x, -int( floor( log10( x ) ) - ( n - 1 ) ) )
+   frmtString = '{}'.format(n)
+   frmtString = "{:."+frmtString+"g}"
+   return float( frmtString.format(x) )
+   # Old Code
+   try:
+      return round( x, -int( floor( log10( x ) ) - ( n - 1 ) ) )
+   except (ValueError) :
+      print "X:",x
+      print "N:",n
+      raise ValueError
 
 def D20( ):
    sides = range(1,21)
@@ -34,4 +43,12 @@ def GetTruth( prompt="> " ):
          continue
 
 if __name__ == '__main__' :
-   print D20()
+
+   import time
+
+   n=3
+   for i in range(1000,1000000,100):
+      print Round_to_n( i, n )
+      
+      time.sleep(0.01)
+
