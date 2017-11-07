a388efddbf86f5c94d92eb49919fa804218a7a86
John Murray <jmurrayufo@gmail.com>
2017-11-07 11:57:19 -0700

    Basic Armor template

--- CharFolio/code/Item/Armor.py
+++ CharFolio/code/Item/Armor.py
@@ -1,2 +1,22 @@
+
+import logging
+import json
+
 from . import Item
 
+class Armor(Item.Item):
+
+    logger = logging.getLogger("charFolio").getChild(__module__)
+
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.armor_bonus = kwargs.get("armor_bonus",1)
+        self.shield_bonus = kwargs.get("shield_bonus",None)
+        self.max_dex_bonus = kwargs.get("max_dex_bonus",8)
+        self.ac_penalty = kwargs.get("ac_penalty",0)
+        self.arcane_spell_failure = kwargs.get("arcane_spell_failure",0.05)
+
+
+    def save(self, file_):
+        with open(file_,'w') as fp:
+            json.dump(self.__dict__, fp, sort_keys=True, indent=2)
--- CharFolio/code/__init__.py
+++ CharFolio/code/__init__.py
@@ -3,5 +3,6 @@ from .Bonus.Bonus import Bonus
 from .Character.Character import Character
 from .Character.God import God
 from .Item.Item import Item
+from .Item.Armor import Armor
 from .Item.Weapon import Weapon
 from .Skill.Skill import Skill
