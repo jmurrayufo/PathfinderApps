080b3e03898a94be9bc91fa8cf4b6b74146ab9be
John Murray <jmurrayufo@gmail.com>
2017-11-09 15:56:35 -0700

    Initial copy of Balancer

--- Balancer/Balancer.py
+++ Balancer/Balancer.py
@@ -1,4 +1,174 @@
 #!/usr/bin/env python3
 
-print("Hello world")
+import SQL
+import argparse
+import json
+
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--add-player', nargs=2,
+                    help='Add a new player')
+parser.add_argument('--list-players', action='store_true',
+                    help='List off players')
+parser.add_argument('--loot-template', action='store_true',
+                    help='Create a loot template')
+parser.add_argument('--split-loot', nargs=1,
+                    help='Create a loot template')
+parser.add_argument('-p','--party-fund', action='store_true',
+                    help='Should we fund the party?')
+
+args = parser.parse_args()
+# print(args)
+sql = SQL.SQL()
+
+if args.add_player:
+    sql.add_player(args.add_player[0],args.add_player[1])
+
+
+if args.list_players:
+    players = sql.get_players()
+    balances = sql.get_balances()
+
+    for player in players:
+        print(f"'{player['name']}' playing '{player['character_name']}' with {balances[player['player_id']]:,.2f} gp")
+
+
+if args.loot_template:
+    template = {}
+    
+    template['coins'] = {}
+    template['coins']['platinum'] = 0
+    template['coins']['gold'] = 0
+    template['coins']['silver'] = 0
+    template['coins']['copper'] = 0
+
+    template['items'] = []
+    template['items'].append({"name":"sword","value":50,"amount":1})
+    template['items'].append({"name":"potion","value":50,"amount":6})
+
+    template['player_loot'] = {}
+    players = sql.get_players()
+    for player in players:
+        template['player_loot'][player['name']] = 0
+
+    with open("template.json",'w') as fp:
+        json.dump(template,fp,indent=4)
+
+
+if args.split_loot:
+    with open(args.split_loot[0],'r') as fp:
+        data = json.load(fp)
+    
+    balances = sql.get_balances()
+    party_id = sql.get_player_id('party')    
+    print("Initial Player Balances")
+    for player_id in balances:
+        print(f"{sql.get_player(player_id)['name']:>8}:{balances[player_id]:7,.2f} gp")
+
+
+    total_loot_value = 0
+    for item in data['items']:
+        total_loot_value += item['amount']*item['value']
+    total_loot_value += data['coins']['platinum']*10
+    total_loot_value += data['coins']['gold']*1
+    total_loot_value += data['coins']['silver']/10
+    total_loot_value += data['coins']['copper']/100
+
+    remaining_loot_value = total_loot_value
+    print(f"\nTotal loot to split {total_loot_value:.2f} gp.")
+
+    # We need to keep track of the actual COINS that we split
+    coin_payouts = {}
+    for player_id in balances:
+        coin_payouts[player_id] = 0
+
+    # First things first, players buy their loot
+    print("\nPlayer Loot Taken")
+    for player in data['player_loot']:
+        player_id = sql.get_player_id(player)
+        print(f"{player:>8}:{data['player_loot'][player]:7,.2f} gp")
+        balances[player_id] += data['player_loot'][player]
+        remaining_loot_value -= data['player_loot'][player]
+
+    print("\nBalance After Player Loot")
+    for player_id in balances:
+        print(f"{sql.get_player(player_id)['name']:>8}:{balances[player_id]:7,.2f} gp")
+
+
+    party_balanced = False
+    print("\nAllocate Loot until party is balanced, or loot is gone.")
+    while party_balanced == False:
+        print("\nLoop to equal out players.")
+        # Find the lowest balance
+        balances_list = list(set([balances[player_id] for player_id in balances if player_id != party_id]))
+        if len(balances_list) == 1:
+            print("Players are equal, split remaining loot.")
+            break
+        balances_list = sorted(balances_list)
+
+        lowest_balance = balances_list[0]
+        second_lowest_balance = balances_list[1]
+
+        print(f"Lowest balance is {lowest_balance:.2f} gp")
+        print(f"Target for these balances is {second_lowest_balance:.2f} gp")
+        
+        print("Who has this amount?")
+        lowest_players = []
+        for player_id in balances:
+            if player_id == party_id:
+                continue
+            if balances[player_id] == lowest_balance:
+                lowest_players.append(player_id)
+
+        print(f"Found {len(lowest_players)} players with {lowest_balance:.2f} gp")
+        for player_id in lowest_players:
+            print(f"  {sql.get_player(player_id)['name']}")
+
+        # Check to make sure we can PAY this amount!
+        if remaining_loot_value < len(lowest_players) * (second_lowest_balance - lowest_balance):
+            print("\nThere is not enough remaining gold to balance the groups!")
+            print("Pay them what we can.")            
+            for player_id in lowest_players:
+                balances[player_id] += remaining_loot_value/len(lowest_players)
+                coin_payouts[player_id] += remaining_loot_value/len(lowest_players)
+            remaining_loot_value = 0
+            break
+        else: 
+            print(f"Pay out {second_lowest_balance - lowest_balance:.2f} gp to each player")
+            for player_id in lowest_players:
+                balances[player_id] += second_lowest_balance - lowest_balance
+                coin_payouts[player_id] += second_lowest_balance - lowest_balance
+                remaining_loot_value -= second_lowest_balance - lowest_balance
+
+    if remaining_loot_value > 0:    
+        print(f"\nWe now have {remaining_loot_value:.2f} gp to split.")
+
+        split_number = len(balances) - 1
+        if args.party_fund:
+            split_number += 1
+
+        print(f"Give each remaining member {remaining_loot_value/split_number:.2f} gp")
+        for player_id in balances:
+            if not args.party_fund and player_id == party_id:
+                continue
+            balances[player_id] += remaining_loot_value/split_number
+            coin_payouts[player_id] += remaining_loot_value/split_number
+
+    print("\nFinal Results")
+    old_balances = sql.get_balances()
+    transactions = {}
+    for player_id in balances:
+        if old_balances[player_id] != balances[player_id]:
+            transactions[player_id] = balances[player_id] - old_balances[player_id]
+
+        print(f"{sql.get_player(player_id)['name']:>8}")
+        print(f"        - Old Balance: {old_balances[player_id]:7,.2f} gp")
+        print(f"        - New Balance: {balances[player_id]:7,.2f} gp")
+        print(f"        - Coins Paid:  {coin_payouts[player_id]:7,.2f} gp")
+
+
+    print("Do you wish to commit this to the DB?")
+    if input("(y/n)> ").lower().startswith("y"):
+        for transaction in transactions:
+            sql.add_transaction(transaction,data['name'],transactions[transaction])
 
--- Balancer/SQL.py
+++ Balancer/SQL.py
@@ -6,6 +6,7 @@ import shutil
 import sqlite3
 import time
 import uuid
+import datetime
 
 class SQL:
     c = None
@@ -40,28 +41,35 @@ class SQL:
     def initalize(self):
         # Init the connection
 
-        self.conn = sqlite3.connect('local_cache.db')
+        self.conn = sqlite3.connect('party.db')
         self.conn.row_factory = self.dict_factory
         self.c = self.conn.cursor()
 
         # Add tables as needed
-        self.log.info("Create transaction table")
-        self.c.execute("""
-            CREATE TABLE IF NOT EXISTS transaction
-            (
-                player_id INTEGER UNIQUE ON CONFLICT FAIL NOT NULL,
-                name TEXT,
-                amount
-            )
-            """)
-
         self.log.info("Create players table")
         self.c.execute("""
-            CREATE TABLE IF NOT EXISTS regions
+            CREATE TABLE IF NOT EXISTS players
             (
-                player_id INTEGER UNIQUE ON CONFLICT FAIL NOT NULL,
+                player_id TEXT UNIQUE ON CONFLICT FAIL NOT NULL,
                 name TEXT UNIQUE,
                 character_name TEXT
+            )
+            """)
+        self.c.execute("""
+            INSERT INTO players 
+            (player_id,name,character_name)
+            VALUES
+            (?,?,?)
+            """,(str(uuid.uuid1()),"party","party"))
+
+        self.log.info("Create transactions table")
+        self.c.execute("""
+            CREATE TABLE IF NOT EXISTS transactions
+            (
+                player_id TEXT NOT NULL,
+                amount NUMERIC,
+                datetime TEXT,
+                name TEXT
             )
             """)
 
@@ -70,7 +78,18 @@ class SQL:
 
     def get_players(self):
         self.c.execute("SELECT * FROM players")
-        return self.c.fetchall
+        return self.c.fetchall()
+
+
+    def get_player(self, player_id):
+        self.c.execute("SELECT * FROM players WHERE player_id=?",(player_id,))
+        return self.c.fetchone()
+
+
+    def get_player_id(self, name):
+        self.c.execute("SELECT * FROM players WHERE name=?",(name,))
+        return self.c.fetchone()['player_id']
+
 
     def add_player(self, name, character_name):
         sqlCmd = f"""
@@ -79,196 +98,37 @@ class SQL:
             VALUES 
             (?,?,?)
         """
-        self.c.execute(sqlCmd,(uuid.UUID(),name,character_name))
+        self.c.execute(sqlCmd,(str(uuid.uuid1()),name,character_name))
+        self.conn.commit()
 
-class Foo:
-    ##################
-    ## Item methods ##
-    ##################
-    def _item_make_good(self, item):
-        """Take wide range of valid inputs, turn into an Item
-        """
-        # TODO Finish this!
-        if isinstance(item,int):
-            return Item.Item(type_id=item)
-        elif isinstance(item,str):
-            self.c.execute("SELECT * FROM items WHERE name=?",(item,))
-        elif isinstance(item,str):
-            self.c.execute("SELECT * FROM items WHERE name=?",(item,))
-        elif isinstance(item,Item.Item):
-            if item.type_id is not None:
-                self.c.execute("SELECT * FROM items WHERE type_id=?",(item.type_id,))
-            elif item.name is not None:
-                self.c.execute("SELECT * FROM items WHERE name=?",(item.name,))
-            else:
-                ValueError(f"Item {item} didn't have a type_or or name")
-        elif isinstance(item,dict):
-            if 'type_id' in item and item['type_id'] is not None:
-                self.c.execute("SELECT * FROM items WHERE type_id=?",(item['type_id'],))
-            if 'name' in item and item['name'] is not None:
-                self.c.execute("SELECT * FROM items WHERE name=?",(item['name'],))
-            else:
-                ValueError(f"dict {item} didn't have a type_or or name")
-        else:
-            raise TypeError(f"Item {item} is of type {type(item)}, not {Item.Item}")
 
-    def clean_items(self):
-        """Drop items from the items table
+    def get_balances(self):
+        self.c.execute("SELECT * FROM transactions")
+        transactions = self.c.fetchall()
+        players = self.get_players()
+        ret_val = {}
+        for player in players:
+            ret_val[player['player_id']] = 0
 
-        Keyword Arguments:
-        filter -- If given, function to return true on rows that are valid
-        """
-        self.c.execute("DELETE FROM items")
+        for transaction in transactions:
+            ret_val[transaction['player_id']] += transaction['amount']
+        return ret_val
 
-    def get_item(self, item):
-        """Try to get item from db
 
-        Keyword Arguments:
-        item -- Accepts several types, and will check the db accordingly
-            dict() -- Assumed to be a dict that has a type_id or name key
-            int() -- Assumed to be a type_id
-            Item() -- Assume to be an Item class, will check item.id then item.name
-            str() -- Assume to be the full name
+    def add_transaction(self, player_id, name, amount):
 
-        Returns:
-        Item()
-
-        Raises:
-        KeyError - Given item was not in the DB
-        TypeError - Given argument isn't a valid type
-        ValueError - Given item is not in the db
-        """
-        if isinstance(item,int):
-            self.c.execute("SELECT * FROM items WHERE type_id=?",(item,))
-        elif isinstance(item,str):
-            self.c.execute("SELECT * FROM items WHERE name=?",(item,))
-        elif isinstance(item,str):
-            self.c.execute("SELECT * FROM items WHERE name=?",(item,))
-        elif isinstance(item,Item.Item):
-            if item.type_id is not None:
-                self.c.execute("SELECT * FROM items WHERE type_id=?",(item.type_id,))
-            elif item.name is not None:
-                self.c.execute("SELECT * FROM items WHERE name=?",(item.name,))
-            else:
-                ValueError(f"Item {item} didn't have a type_or or name")
-        elif isinstance(item,dict):
-            if 'type_id' in item and item['type_id'] is not None:
-                self.c.execute("SELECT * FROM items WHERE type_id=?",(item['type_id'],))
-            if 'name' in item and item['name'] is not None:
-                self.c.execute("SELECT * FROM items WHERE name=?",(item['name'],))
-            else:
-                ValueError(f"dict {item} didn't have a type_or or name")
-        else:
-            raise TypeError(f"Item {item} is of type {type(item)}, not {Item.Item}")
-
-        data = self.c.fetchall()
-        if len(data) == 0:
-            raise ValueError
-        return data[0]
-
-    def get_items(self, filter=None):
-        """Check if given item is in the db
-
-        Keyword Arguments:
-        filter -- If given, function to return true on rows that are valid
-
-        Returns:
-        [Item()...] 
-
-        Raises:
-        TypeError - Filter was given, but is not a valid function
-        ValueError - Given item didn't have an id or name
-        """
-        self.c.execute("SELECT * FROM items")
-        data = self.c.fetchall()
-        return data
-
-    def has_item(self, item):
-        """Check if given item is in the db
-
-        Keyword Arguments:
-        item -- Accepts several types, and will check the db accordingly
-            dict() -- Assumed to be a dict that has a type_id or name key
-            int() -- Assumed to be a type_id
-            Item() -- Assumed to be an Item class, will check item.id then item.name
-            str() -- Assumed to be the full name
-
-        Returns:
-        bool() -- True/False if item is in the db
-
-        Raises:
-        TypeError - Given argument isn't a valid type
-        ValueError - Given item didn't have an id or name
-        """
-        if isinstance(item,int):
-            self.c.execute("SELECT * FROM items WHERE type_id=?",(item,))
-        elif isinstance(item,str):
-            self.c.execute("SELECT * FROM items WHERE name=?",(item,))
-        elif isinstance(item,str):
-            self.c.execute("SELECT * FROM items WHERE name=?",(item,))
-        elif isinstance(item,Item.Item):
-            if item.type_id is not None:
-                self.c.execute("SELECT * FROM items WHERE type_id=?",(item.type_id,))
-            elif item.name is not None:
-                self.c.execute("SELECT * FROM items WHERE name=?",(item.name,))
-            else:
-                ValueError(f"Item {item} didn't have a type_or or name")
-        elif isinstance(item,dict):
-            if 'type_id' in item and item['type_id'] is not None:
-                self.c.execute("SELECT * FROM items WHERE type_id=?",(item['type_id'],))
-            if 'name' in item and item['name'] is not None:
-                self.c.execute("SELECT * FROM items WHERE name=?",(item['name'],))
-            else:
-                ValueError(f"dict {item} didn't have a type_or or name")
-        else:
-            raise TypeError(f"Item {item} is of type {type(item)}, not {Item.Item}")
-
-        data = self.c.fetchall()
-        if len(data) == 0:
-            return False
-        return True
-
-    def insert_item(self, item):
-        """Check if given item is in the db
-
-        Keyword Arguments:
-        item -- Accepts several types, and will check the db accordingly
-            dict() -- Valid dict with at least a type_id
-            Item() -- Assume to be an Item class, will check item.id then item.name
-
-        Returns:
-        None
-
-        Raises:
-        TypeError - Given argument isn't a valid type
-        ValueError - Given item didn't have an id or name
-        """        
-        self.log.debug(f"Inserting {item}")
-
-        if isinstance(item,Item.Item):
-            item = item.dict()
-        elif not isinstance(item,dict):
-            raise TypeError
-
-        self.log.debug(f"Inserting {item['name']}")
-        if 'dogma_attributes' in item:
-            item['dogma_attributes'] = json.dumps(item['dogma_attributes'])
-        if 'dogma_effects' in item:
-            item['dogma_effects'] = json.dumps(item['dogma_effects'])
-
-        keys = ",".join([x for x in item])
-        values = [item[x] for x in item]
-        questionMarks = ",".join( list( "?" * len(item) ) )
         sqlCmd = f"""
-            INSERT OR FAIL INTO items 
-            ({keys})
+            INSERT OR FAIL INTO transactions 
+            (player_id,name,amount,datetime)
             VALUES 
-            ({questionMarks})
+            (?,?,?,?)
         """
-        # self.log.debug(sqlCmd)
-        try:
-            self.c.execute(sqlCmd, values)
-        except sqlite3.IntegrityError:
-            self.log.error(f"Failed to insert {item}")
-            self.log.error("Continuing")
+        self.c.execute(sqlCmd,
+            (player_id,
+            name,
+            amount,
+            datetime.datetime.now(),)
+            )
+        self.conn.commit()
+
 
